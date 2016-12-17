729f86e09270a73b998b5543d8acfea3c46809b5
Ahnup Singh <Ahnup Singh>
2016-12-17 10:05:36 +0545

    TabLayout and Navigation Drawer

--- app/src/main/java/com/example/anupkumarsingh/bmap/MainActivity.java
+++ app/src/main/java/com/example/anupkumarsingh/bmap/MainActivity.java
@@ -1,15 +1,26 @@
 package com.example.anupkumarsingh.bmap;
 
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawerLayout;
-    ActionBarDrawerToggle actionBarDrawerToggle;
+    ActionBarDrawerToggle actionBarDrawerToggle; //HamBurger Icon on the toolbar for the navigation bar
+    FragmentTransaction fragmentTransaction;
+    NavigationView navigationView;
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +32,48 @@ public class MainActivity extends AppCompatActivity {
 
         actionBarDrawerToggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.drawer_open,R.string.drawer_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
+
+        //add framelayout in the home screen
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_container,new HomeFragment());
+        fragmentTransaction.commit();
+        //getSupportActionBar().setTitle("Home");
+
+        navigationView = (NavigationView)findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener(){
+            @Override
+                    public boolean onNavigationItemSelected(MenuItem item){
+                switch (item.getItemId()) {
+                    case R.id.home_id :
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new HomeFragment());
+                        fragmentTransaction.commit();
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+
+                    case R.id.backup_id :
+                        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                        fragmentTransaction.replace(R.id.main_container, new BackupFragment());
+                        fragmentTransaction.commit();
+                        item.setChecked(true);
+                        drawerLayout.closeDrawers();
+                        break;
+                }
+
+                return false;
+            }
+        });
+
+        tabLayout = (TabLayout)findViewById(R.id.tablayout);
+        viewPager = (ViewPager)findViewById(R.id.viewPager);
+
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        viewPagerAdapter.addFragements(new ReceivableFragment(),"Receivable");
+        viewPagerAdapter.addFragements(new PayableFragment(),"Payable");
+        viewPagerAdapter.addFragements(new Wages(),"Wages");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
     @Override
---
+++ app/src/main/java/com/example/anupkumarsingh/bmap/ViewPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.anupkumarsingh.bmap;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Anup Kumar Singh on 12/16/2016.
+ */
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    ArrayList<Fragment> fragments = new ArrayList<>();
+    ArrayList<String> tabTitles = new ArrayList<>();
+
+    public void addFragements(Fragment fragments, String titles)
+    {
+        this.fragments.add(fragments);
+        this.tabTitles.add(titles);
+    }
+    public ViewPagerAdapter(FragmentManager fm)
+    {
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position){
+        return fragments.get(position);
+    }
+
+    @Override
+    public int getCount(){
+        return fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return tabTitles.get(position);
+    }
+
+}
